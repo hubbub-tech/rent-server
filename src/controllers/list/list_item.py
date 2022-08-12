d1df4dcf9c264903d62df4d47ad420ada25328ef
Ade Balogun <abalogun316@gmail.com>
2022-08-12 08:48:40 -0400

    refactor listing services

--- src/controllers/list/list_item.py
+++ src/controllers/list/list_item.py
@@ -1,71 +1,70 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request
 
 bp = Blueprint("list", __name__)
 
 
-@bp.post('/list/submit')
+@bp.post('/list')
 @login_required
-def list_submit():
-    format = "%Y-%m-%d"
-    flashes = []
-    data = request.form
-    if data:
-        if data.get("startDate") and data.get("endDate"):
-            new_date_started = json_date_to_python_date(data["startDate"])
-            new_date_ended = json_date_to_python_date(data["endDate"])
-            form_data = {
-                "item": {
-                    "lister_id": g.user_id,
-                    "name": data["name"],
-                    "price": data["price"],
-                    "address_line_1": data["line_1"],
-                    "address_line_2": data["line_2"],
-                    "address_zip": data["zip"]
-                },
-                "details": {
-                    "description": data["description"],
-                    "condition": data["condition"],
-                    "volume": data["volume"],
-                    "weight": data["weight"],
-                    "id": None
-                },
-                "calendar": {
-                    "date_started": new_date_started,
-                    "date_ended": new_date_ended,
-                    "id": None
-                },
-                "address": {
-                    "line_1": data["line_1"],
-                    "line_2": data["line_2"],
-                    "city": data["city"],
-                    "state": data["state"],
-                    "zip": data["zip"]
-                },
-                "tags": ['all'] + data['tags'].split(","),
-                "is_listed_from_user_address": strtobool(data["isDefaultAddress"])
-            }
-            image = request.files["image"]
-            form_check = validate_listing(form_data)
-            if form_check["is_valid"]:
-                item = create_item(form_data)
-                image_data = {
-                    "self": item,
-                    "image" : image,
-                    "directory" : "items",
-                    "bucket" : AWS.S3_BUCKET
-                }
-                upload_response = upload_image(image_data)
-                if upload_response["is_valid"]:
-                    email_data = get_new_listing_email(item)
-                    send_async_email.apply_async(kwargs=email_data)
-                    flashes.append(form_check["message"])
-                    return {"flashes": flashes}, 200
-                else:
-                    flashes.append(upload_response["message"])
-            else:
-                flashes.append("There was an error getting the dates you set, make sure they're in 'MM/DD/YYYY'.")
-        else:
-            flashes.append("No data was sent.")
-    else:
-        flashes.append("No data was sent! Try again.")
-    return {"flashes": flashes}, 406
+def list_item():
+
+    try:
+        item_data = {
+            "name": request.json["item"]["name"],
+            "retail_price": request.json["item"]["retailPrice"],
+            "description": request.json["item"]["description"],
+            "weight": request.json["item"].get("weight"),
+            "weight_unit": request.json["item"].get("weightUnit"),
+            "dim_height": request.json["item"].get("height"),
+            "dim_length": request.json["item"].get("length"),
+            "dim_width": request.json["item"].get("width"),
+            "manufacturer_id": request.json["item"].get("manufacturerId"),
+            "lister_id": g.user_id,
+            "address_line_1": None,
+            "address_line_2": None,
+            "address_country": None,
+            "address_zip": None
+        }
+
+        address_data = {
+            "line_1": request.json["address"]["lineOne"],
+            "line_2": request.json["address"]["lineTwo"],
+            "city": request.json["address"]["city"],
+            "state": request.json["address"]["state"],
+            "country": request.json["address"]["country"],
+            "zip": request.json["address"]["zip"]
+        }
+
+        calendar_data = {
+            "dt_started": request.json["calendar"]["dtStarted"],
+            "dt_ended": request.json["calendar"]["dtEnded"]
+        }
+
+        tags = ["all"] + request.json.get("tags").split()
+        is_from_lister_address = strtobool(request.json["isDefaultAddress"])
+
+    except KeyError:
+        errors = ["Missing data to complete your listing! Please, try again."]
+        response = make_response({ "messages": errors }, 401)
+        return response
+    except Exception as e:
+        errors = ["Something went wrong. Please, try again."]
+        # NOTE: Log error here.
+        response = make_response({ "messages": errors }, 500)
+        return response
+
+    form_check = validate_calendar(calendar_data)
+
+    if not form_check["is_valid"]:
+        errors = form_check["messages"]
+        response = make_response({ "messages": errors }, 403)
+        return response
+
+    address = create_address(address_data)
+    new_item = create_item(item_data, calandar_data)
+    email_data = get_successful_listing_email(new_item)
+    send_async_email.apply_async(kwargs=email_data)
+
+    messages = ["Thanks for listing on Hubbub!"]
+
+    response = make_response({ "messages": messages }, 200)
+    return response
